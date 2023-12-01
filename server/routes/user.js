7f65806df6467abb0fd1af955e9dbc57d887cbc8
Quang Minh <vigogroupdev7@gmail.com>
2023-12-01 18:13:58 +0700

    mern_stack3

---
+++ server/routes/user.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const ctrls = require('../controllers/user')
+const { verifyAccessToken } = require('../middlewares/verifyToken')
+
+router.post('/register', ctrls.register)
+router.post('/login', ctrls.login)
+router.get('/current', verifyAccessToken, ctrls.getCurrent)
+router.post('/refreshtoken', ctrls.refreshAccessToken)
+router.get('/logout', ctrls.logout)
+router.get('/forgotpassword', ctrls.forgotPassword)
+router.put('/resetpassword', ctrls.resetPassword)
+module.exports = router
+
+//CRUD | Create - Read - Update - Delete | POST - GET - PUT - DELETE
